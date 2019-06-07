d8e809dff842d0f6983c625f981edefd0ef090e2
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-06-07 10:30:44 -0800

    database connection was completed

--- api.py
+++ api.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 09:36:55 2019
 
+@author: Tadele Y
+"""
+
+from DBcm import useDatabase, ConnectionError, CredentialsError
 from flask import Flask, request, jsonify
+from flask import copy_current_request_context
+from threading import Thread
+from time import sleep 
 app = Flask(__name__)
+app.config['dbconfig'] = {'host':'127.0.0.1', 'user':'root', 'database':'android',}
 studentDB = [{'id':'1', 'name':'tade'},{'id':'2', 
              'name':'jo_the_tall'},{'id':'3', 'name':'tiny_jo'}]
+studentDBR = '''select first_name, last_name, course, section, department, password from student '''
 
 @app.route('/')
 def index():
@@ -10,7 +22,10 @@ def index():
 
 @app.route('/stddb/student', methods=['GET'])
 def get_all_student():
-    return jsonify({'std': studentDB})
+    with useDatabase(app.config['dbconfig']) as cursor :
+        cursor.execute(studentDBR)
+        contents = cursor.fetchall()  
+    return jsonify(contents)
 
 @app.route('/stddb/student/<stdid>', methods= ['GET'])
 def get_student(stdid):
@@ -26,13 +41,15 @@ def update_std(stdid):
     return jsonify({'std':std[0]})
 
 @app.route('/stddb/student',methods=['POST'])
-def create_student(): 
-    dat = {
-    'id' : request.json['id'], 
-    'name' :  request.json['name']
-    }
-    studentDB.append(dat)
-    return jsonify(dat)
+def create_student():
+    with useDatabase(app.config['dbconfig']) as cursor :
+        _SQL = '''insert into student (first_name, last_name, course, section, department, password) values (%s,%s,%s,%s,%s,%s) '''
+        cursor.execute(_SQL,(str(request.json['first_name']), str(request.json['last_name']), str(request.json['course']), str(request.json['section']), str(request.json['department']), str(request.json['password'] )))
+        studentDBR = '''select first_name, last_name, course, section, department, password from student '''
+        cursor.execute(studentDBR)
+        contents = cursor.fetchall()  
+    return jsonify(contents)
+    
 
 
 @app.route('/stddb/student/<stdid>',methods=['DELETE'])
