e5b10d6105c62232dd42a7397c3abe3b612c9350
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-06-08 12:11:23 -0800

    json file key value pair error fixed

--- api.py
+++ api.py
@@ -10,6 +10,7 @@ from flask import Flask, request, jsonify
 from flask import copy_current_request_context
 from threading import Thread
 from time import sleep 
+import json
 app = Flask(__name__)
 app.config['dbconfig'] = {'host':'127.0.0.1', 'user':'root', 'database':'android',}
 
@@ -20,27 +21,39 @@ def index():
 
 @app.route('/stddb/student', methods=['GET'])
 def get_all_student():
+    response = ['first_name', 'last_name', 'course', 'section', 'department']
+    data = []
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select first_name, last_name, course, section, department, password from student '''
         cursor.execute(_SQL)
-        contents = cursor.fetchall()  
-    return jsonify(contents)
+        contents = cursor.fetchall()
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
 @app.route('/stddb/student/<course>', methods= ['GET'])
 def get_student(course):
+    response = ['first_name', 'last_name']
+    data = [] 
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select first_name, last_name from student where course = %s '''
         cursor.execute(_SQL, (course,))
         contents = cursor.fetchall()
-    return jsonify(contents)
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
-@app.route('/stddb/student/<id>', methods= ['GET'])
+@app.route('/stddb/student/id/<id>', methods= ['GET'])
 def get_student_by_course(id):
+    response = ['first_name', 'last_name', 'course', 'section', 'department']
+    data = [] 
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select first_name, last_name, course, section, department from student where id = %s '''
         cursor.execute(_SQL, (id,))
         contents = cursor.fetchall()
-    return jsonify(contents)
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
 
 @app.route('/stddb/student/<id>',methods=['PUT'])
@@ -61,19 +74,13 @@ def update_std(id):
         if 'course' in request.json:
             _SQL = '''update student set course = %s where id = %s '''
             cursor.execute(_SQL, (request.json['course'],id))
-        _SQL = '''select first_name, last_name, course, section, department from student where id = %s '''
-        cursor.execute(_SQL, (id,))
-        contents = cursor.fetchall() 
-    return jsonify(contents)
+    return jsonify({'response':'Success'})
 @app.route('/stddb/student',methods=['POST'])
 def create_student():
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''insert into student (first_name, last_name, course, section, department, password) values (%s,%s,%s,%s,%s,%s) '''
-        cursor.execute(_SQL,(str(request.json['first_name']), str(request.json['last_name']), str(request.json['course']), str(request.json['section']), str(request.json['department']), str(request.json['password'] )))
-        studentDBR = '''select first_name, last_name, course, section, department, password from student '''
-        cursor.execute(studentDBR)
-        contents = cursor.fetchall()  
-    return jsonify(contents)
+        cursor.execute(_SQL,(str(request.json['first_name']), str(request.json['last_name']), str(request.json['course']), str(request.json['section']), str(request.json['department']), str(request.json['password'] ))) 
+    return jsonify({'response':'Success'})
     
 
 
@@ -91,48 +98,54 @@ operations for taking attendace
 def add_attend_student():
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''insert into attendance (first_name,last_name, status, date) values (%s,%s,%s,%s) '''
-        cursor.execute(_SQL,(str(request.json['first_name']), str(request.json['last_name']), request.json['status'], str(request.json['date'])))
-        _SQL = '''select first_name, last_name, status, date from attendance '''
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()  
-    return jsonify(contents)
+        cursor.execute(_SQL,(str(request.json['first_name']), str(request.json['last_name']), request.json['status'], str(request.json['date']))) 
+    return jsonify({'response':'Success'})
 
 @app.route('/stddb/attendance', methods=['GET'])
 def get_attendance():
+    response = ['first_name', 'last_name', 'status']
+    data = [] 
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select first_name, last_name, status, date from attendance '''
         cursor.execute(_SQL)
         contents = cursor.fetchall()  
-    return jsonify(contents)
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
 '''
 Operations for teachers
 '''
 @app.route('/stddb/teacher', methods=['GET'])
 def get_teacher():
+    response = ['name', 'course']
+    data = [] 
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select name, course from teacher '''
         cursor.execute(_SQL)
         contents = cursor.fetchall()  
-    return jsonify(contents)
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
 @app.route('/stddb/teacher', methods=['POST'])
 def add_teacher():
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''insert into teacher (name,password, course) values (%s,%s,%s) '''
         cursor.execute(_SQL,(str(request.json['name']), str(request.json['password']), str(request.json['course'])))
-        _SQL = '''select name, course from teacher '''
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()  
-    return jsonify(contents)
+    return jsonify({'response':'Success'})
 
 @app.route('/stddb/teacher/login', methods=['POST'])
 def login_teacher():
+    response = ['name']
+    data = [] 
     with useDatabase(app.config['dbconfig']) as cursor :
         _SQL = '''select name from  teacher where (name = %s and password = %s) '''
         cursor.execute(_SQL,(str(request.json['name']), str(request.json['password'])))
         contents = cursor.fetchall()  
-    return jsonify(contents)
+    for content in contents :
+        data.append(dict(zip(response, content)))
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
